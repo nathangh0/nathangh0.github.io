5b7c53bbefaf418817a8f1e6f794d1107775bcc3
= <=>
2024-06-17 17:39:30 -0500

    code cleanup file splitting theme update

---
+++ assets/scripts/home-script.js
@@ -0,0 +1,9 @@
+function open_mail()
+{
+    window.open('https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=nathanghuang@gmail.com');
+}
+
+function open_git()
+{
+    window.open('https://github.com/nghff'); 
+}
